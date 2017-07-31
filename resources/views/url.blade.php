f14591d741bc9f32c59b0e45278b013bd21c8b94
Mikica <mikica.ivosevic@golive.rs>
2017-07-31 15:49:19 +0200

    fix spaces

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -1,19 +1,21 @@
 <url>
     @if (! empty($tag->url))
-    <loc>{{ $tag->url }}</loc>@endif
+        <loc>{{ $tag->url }}</loc>
+    @endif
 
-@if (! empty($tag->xhtmlLinks))
-@foreach($tag->xhtmlLinks as $xhtmlLinkAttrs)
-         @include('laravel-sitemap::xhtml-link', ['xhtmlLinksAttrs' => $xhtmlLinkAttrs])
-@endforeach
-@endif
+    @foreach($tag->xhtmlLinks as $xhtmlLinkAttrs)
+        @include('laravel-sitemap::xhtml-link', ['xhtmlLinksAttrs' => $xhtmlLinkAttrs])
+    @endforeach
+
     @if (! empty($tag->lastModificationDate))
-    <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>@endif
+        <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
+    @endif
 
     @if (! empty($tag->changeFrequency))
-    <changefreq>{{ $tag->changeFrequency }}</changefreq>@endif
+        <changefreq>{{ $tag->changeFrequency }}</changefreq>
+    @endif
 
     @if (! empty($tag->priority))
-    <priority>{{ $tag->priority }}</priority>
+        <priority>{{ $tag->priority }}</priority>
     @endif
 </url>
